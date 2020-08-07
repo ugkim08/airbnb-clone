562286542431bb85cc97b97570c7d28ac8fe2b31
ugkim08 <wigk0118@gmail.com>
2020-08-07 17:57:22 +0900

    user/models 업뎃
    admin/users/user/add/에 avatar, gender, bio 영역 추가

--- users/models.py
+++ users/models.py
@@ -6,4 +6,18 @@ from django.db import models
 
 class User(AbstractUser):
 
-    bio = models.TextField(default="")
+    """ Custom User Model """
+
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+
+    GENDER_CHOICES = (
+        (GENDER_MALE, "male"),
+        (GENDER_FEMALE, "female"),
+        (GENDER_OTHER, "other"),
+    )
+
+    avatar = models.ImageField(null=True, blank=True)
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=10, null=True, blank=True)
+    bio = models.TextField(default="", blank=True)
