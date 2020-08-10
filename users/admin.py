0ab1db4a343a800aecb0625c100b4724d527fc2e
ugkim08 <wigk0118@gmail.com>
2020-08-10 23:05:57 +0900

    update admin panel
    user admin panel 업뎃함.

--- users/admin.py
+++ users/admin.py
@@ -3,4 +3,11 @@ from . import models
 
 @admin.register(models.User)
 class CustomUserAdmin(admin.ModelAdmin):
-    pass
+    
+    """ Custom User Admin """
+
+    # admin > user panel에 list_display 변수에 있는 항목들을 table에 추가해줌.
+    # list_display = ("username", "email", "gender", "langauge", "currency", "superhost")
+    # list_filter = ("langauge", "currency","superhost",)
+
+    
