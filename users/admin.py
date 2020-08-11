372cf276a5cea6e793e53d5a3c53081c1a45263e
ugkim08 <wigk0118@gmail.com>
2020-08-11 23:26:08 +0900

    update user admin
    django에서 재공하는 어드민 필드와 커스텀 필드를 함께 쓸 수 있도록 수정함.

--- users/admin.py
+++ users/admin.py
@@ -1,13 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from . import models
 
 @admin.register(models.User)
-class CustomUserAdmin(admin.ModelAdmin):
+class CustomUserAdmin(UserAdmin):
     
     """ Custom User Admin """
 
     # admin > user panel에 list_display 변수에 있는 항목들을 table에 추가해줌.
     # list_display = ("username", "email", "gender", "langauge", "currency", "superhost")
     # list_filter = ("langauge", "currency","superhost",)
-
-    
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "birthdate",
+                    "langauge",
+                    "currency",
+                    "superhost",
+                )
+            }
+        ),
+    )
