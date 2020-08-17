95a235a1908a86490f9e6ad5a6bd8da915b01a5f
ugkim08 <wigk0118@gmail.com>
2020-08-17 19:24:46 +0900

    room/models 업데이트
    admin에 room생성 기능 추가

--- core/models.py
+++ core/models.py
@@ -4,8 +4,8 @@ class TimeStampedModel(models.Model):
 
     """ Time Stamped Model """
 
-    created = models.DateTimeField()
-    updated = models.DateTimeField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
     class Meta:
         abstract = True #DB에 저장되지 않는 코드를 쓸때 abstract를 쓴다.
