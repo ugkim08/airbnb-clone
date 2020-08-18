82f383149e19401bc26c77366522141bb4882784
ugkim08 <wigk0118@gmail.com>
2020-08-18 22:10:58 +0900

    room type추가.
    ForeignKey = 1:N 관계를 의미합니다. 예를 들어 게시글과 댓글을 생각할 수 있습니다
    
    ManyToManyField = 다대다 (N:N)의 관계를 의미함.

--- core/models.py
+++ core/models.py
@@ -4,8 +4,8 @@ class TimeStampedModel(models.Model):
 
     """ Time Stamped Model """
 
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True) # model이 생성된 날짜를 구하려면 auto_now_add를 사용해서 날짜를 구한다.
+    updated = models.DateTimeField(auto_now=True) # 새로운 날짜로 업데이트 해야할 경우 auto_now 를 True로 사용한다.
 
     class Meta:
         abstract = True #DB에 저장되지 않는 코드를 쓸때 abstract를 쓴다.
