a561f30907b083f1834232c0ee5aaf1f7da01ec2
ugkim08 <wigk0118@gmail.com>
2020-08-23 20:05:56 +0900

    add photo
    admin에 photo 추가.

--- rooms/models.py
+++ rooms/models.py
@@ -17,25 +17,40 @@ class AbstractItem(core_models.TimeStampedModel):
 
 class RoomType(AbstractItem):
 
-    pass
+    class Meta:
+        verbose_name = "Room Type"
 
 class Amenity(AbstractItem):
 
     """Amenity RoomType Model Definition"""
 
-    pass
+    class Meta:
+        verbose_name_plural = "Amenities"
 
 class Facility(AbstractItem):
 
     """Facility Model Definition"""
 
-    pass
+    class Meta:
+        verbose_name_plural = "Facilities"
 
 class HouseRule (AbstractItem):
 
     """House Rule Definition"""
 
-    pass
+    class Meta:
+        verbose_name = "House Rule"
+
+class Photo(core_models.TimeStampedModel):
+
+    """Photo Model Definition"""
+
+    caption = models.CharField(max_length=80)
+    file = models.ImageField()
+    room = models.ForeignKey("Room", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.caption
 
 class Room(core_models.TimeStampedModel):
 
@@ -54,11 +69,11 @@ class Room(core_models.TimeStampedModel):
     check_in = models.TimeField()
     check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
-    host = models.ForeignKey(user_models.User, on_delete=models.CASCADE) # on delete : forignkey로 연결된 User를 삭제할 경우 해당 User와 연결된 Room도 함께 삭제된다는 의미이다. CASCADE = 폭포수 효과.
-    room_type = models.ForeignKey(RoomType, on_delete=models.SET_NULL, null=True)
-    amenites = models.ManyToManyField(Amenity)
-    facilites = models.ManyToManyField(Facility)
-    house_rule = models.ManyToManyField(HouseRule)
+    host = models.ForeignKey("users.User", on_delete=models.CASCADE) # on delete : forignkey로 연결된 User를 삭제할 경우 해당 User와 연결된 Room도 함께 삭제된다는 의미이다. CASCADE = 폭포수 효과.
+    room_type = models.ForeignKey("RoomType", on_delete=models.SET_NULL, null=True)
+    amenites = models.ManyToManyField("Amenity", blank=True)
+    facilites = models.ManyToManyField("Facility", blank=True)
+    house_rule = models.ManyToManyField("HouseRule", blank=True)
 
     def __str__(self):
         return self.name
